5572e720019fb650acb283a87d45724f96fe239e
Ashmit Verma <a1verma8105@gmail.com>
2024-10-02 22:16:13 +0530

    commit

--- backend/controllers/passport.js
+++ backend/controllers/passport.js
@@ -27,7 +27,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/auth/google/redirect",
+      callbackURL: "https://spirit24-main.onrender.com/auth/google/redirect",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
