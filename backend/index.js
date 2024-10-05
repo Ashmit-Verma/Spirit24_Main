f70a835b0a48dcac3378598687befd2c3b86eb62
Ashmit Verma <a1verma8105@gmail.com>
2024-10-05 14:04:11 +0530

    commit all

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ server.listen(process.env.PORT, async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection to the database has been established successfully.');
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ alter: false });
     console.log('All models were synchronized successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
